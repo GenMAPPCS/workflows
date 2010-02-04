afb549fd79ee3834d297f6d5146227d16bc54b66
AlexanderPico <apico@gladstone.ucsf.edu>
2010-02-04 00:52:36 +0000

    tweaked to working nicely together

--- src/org/genmapp/workflows/GenMAPPWorkflow.java
+++ src/org/genmapp/workflows/GenMAPPWorkflow.java
@@ -12,27 +12,33 @@ import cytoscape.plugin.CytoscapePlugin;
 
 public class GenMAPPWorkflow extends CytoscapePlugin {
 	public GenMAPPWorkflow() {
-		JMenuItem item = new JMenuItem("GenMAPP Workflow");
-		JMenu pluginMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
-				.getMenu("Plugins");
-		item.addActionListener(new WorkflowsCommandListener(this));
-		pluginMenu.add(item);
+		JDialog d = new JDialog();
+		d.add(new FlowTree());
+		d.pack();
+		d.setVisible(true);
+		d.setAlwaysOnTop(true);
+//		
+//		JMenuItem item = new JMenuItem("GenMAPP Workflow");
+//		JMenu pluginMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
+//				.getMenu("Plugins");
+//		item.addActionListener(new WorkflowsCommandListener(this));
+//		pluginMenu.add(item);
 	}
 
 	// Handles the top-level menu selection event from Cytoscape
-	class WorkflowsCommandListener implements ActionListener {
-		GenMAPPWorkflow plugin = null;
-
-		public WorkflowsCommandListener(GenMAPPWorkflow p) {
-			plugin = p;
-		}
-
-		public void actionPerformed(ActionEvent evt_) {
-			// pop up dialog
-			JDialog d = new JDialog();
-			d.add(new FlowTree());
-			d.pack();
-			d.setVisible(true);
-		}
-	}
+//	class WorkflowsCommandListener implements ActionListener {
+//		GenMAPPWorkflow plugin = null;
+//
+//		public WorkflowsCommandListener(GenMAPPWorkflow p) {
+//			plugin = p;
+//		}
+//
+//		public void actionPerformed(ActionEvent evt_) {
+//			// pop up dialog
+//			JDialog d = new JDialog();
+//			d.add(new FlowTree());
+//			d.pack();
+//			d.setVisible(true);
+//		}
+//	}
 }
