efed7163eb83fea2f9b9519dff22a69e7fc63352
AlexanderPico <apico@gladstone.ucsf.edu>
2010-08-06 23:17:40 +0000

    updated headers

--- src/org/genmapp/workflows/commands/ListCriteriaCommand.java
+++ src/org/genmapp/workflows/commands/ListCriteriaCommand.java
@@ -1,3 +1,18 @@
+/*
+ Copyright 2010 Alexander Pico
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at 
+ 	
+ 	http://www.apache.org/licenses/LICENSE-2.0 
+ 	
+ Unless required by applicable law or agreed to in writing, software 
+ distributed under the License is distributed on an "AS IS" BASIS, 
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ See the License for the specific language governing permissions and 
+ limitations under the License. 
+ */
+
 package org.genmapp.workflows.commands;
 
 import java.awt.event.ActionEvent;
@@ -30,7 +45,7 @@ public class ListCriteriaCommand extends AbstractCommand {
 			FlowTree.flowTree.put(3.1, "setname_1");
 			
 			DefaultMutableTreeNode step3_1 = new DefaultMutableTreeNode(
-					new BuildCriteriaCommand(FlowTree.flowTree.get(3.1)));
+					 new BuildCriteriaCommand(FlowTree.flowTree.get(3.1)));
 			
 			FlowTree.root.add(step3_1);
 			
